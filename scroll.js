99c064398042b8d24fa05efdc59a1451928293f0
pannyumon <pannyum@gmail.com>
2024-12-03 15:18:44 +0900

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -1,8 +1,1 @@
-window.onscroll = function () {
-    const button = document.getElementById('backToTop');
-    button.style.display = window.scrollY > 200 ? 'block' : 'none';
-};
 
-function scrollToTop() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-}
